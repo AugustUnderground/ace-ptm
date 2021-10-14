fd7da61fb38c1e0509dc37b6b8dc778fbb59479d
Yannick Uhlmann <augustunderground@protonmail.com>
2021-10-14 13:36:05 +0200

    individual logs

--- src/test.pl
+++ src/test.pl
@@ -10,7 +10,7 @@ closedir(DIR);
 print "Running OP2 for all Nodes\n";
 
 foreach (@pdks) {
-    system "spectre -I./$_/pdk ./$_/op2/input.scs > /dev/null";
+    system "spectre =l ./src/op2-$_.log -I./$_/pdk ./$_/op2/input.scs";
 
     if ($? == 0) {
         print "[\e[1;32m ✔ \e[0m] No errors for $_ nm PTM devices.\n";
