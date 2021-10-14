65ba111ad98408e2758230f73e69b2673c9047ff
Yannick Uhlmann <augustunderground@protonmail.com>
2021-10-14 13:02:04 +0200

    char script

--- src/char.pl
+++ src/char.pl
@@ -2,13 +2,26 @@
 
 use strict;
 use warnings;
+use experimental 'switch';
 
+use Getopt::Long qw(GetOptions);
+use Scalar::Util qw(looks_like_number);
+
+my $usage = "Usage:\t$0 [--nmos | --pmos] [--tech NODE]\n";
+
+# Read available technology nodes
 opendir(DIR, ".");
-my @pdks = grep(/^[0-9]+$/,readdir(DIR));
+my @all_pdks = grep(/^[0-9]+$/,readdir(DIR));
 closedir(DIR);
 
-print "Characterizing all Nodes\n";
+my @tech;   # Technology Node
+my $nmos;   # NMOS Device
+my $pmos;   # PMOS Device
+my @pdks;   # The nodes in this list will be characterized.
 
+$|++;
+
+# Just Loggin
 sub Log {
     my ($code, $node, $mos) = @_;
     
@@ -19,18 +32,62 @@ sub Log {
     }
 }
 
+# Characterize a primitive device 
+# Char(node, device) => exit_code
 sub Char {
     my ($node, $mos) = @_;
-    system "spectre -I./$node/pdk ./chr/$mos.scs > /dev/null";
+    my $raw_file = "./chr/$node-$mos.raw";
+    my $net_list = "./chr/$mos.scs";
+    my $flags    = "-log -format nutbin";
+    system "spectre $flags -raw $raw_file -I./$node/pdk $net_list > /dev/null";
     return $?;
 }
 
-foreach (@pdks) {
+# Get command line arguments
+GetOptions( 'tech|t=s' => \@tech
+          , 'nmos|n'   => \$nmos
+          , 'pmos|p'   => \$pmos
+          ) or die $usage;
+
+# Characterize either ALL, 1 or NONE.
+given(@tech) {
+    when (!@tech) {  # If no NODE is specified do all.
+        @pdks = @all_pdks;
+    }
+    default {        # Otherwise go through the desired NODEs
+        foreach (@tech) {
+            given ($_) {
+                when(\&looks_like_number) { # If it looks like a number pad it
+                    $_ = sprintf("%03d", $_);
+                    continue;
+                }
+                when(@all_pdks) {           # If a directory of that name exists
+                    @pdks = (@pdks, $_);    # Append the technology node
+                }
+                default {                   # Throw error otherwise
+                    die "@_ is not a valid NODE.\nHas to be ∈ [@all_pdks].\n";
+                }
+            }
+        }
+        print "defined -> @pdks \n";
+    }
+}
+
+# If no device type is given, enable both.
+unless($nmos || $pmos) { ($nmos, $pmos) = (1,1); }
+
+foreach(@pdks) {
     my ($n, $p) = ("nmos", "pmos");
     
-    my $nc = Char($_, $n);
-    Log($nc, $_, $n);
-   
-    my $np = Char($_, $p);
-    Log($np, $_, $p);
+    if($nmos){
+        my $nc = Char($_, $n);
+        Log($nc, $_, $n);
+    }
+
+    if($pmos){
+        my $np = Char($_, $p);
+        Log($np, $_, $p);
+    }
 }
+
+exit 0;
