b6e96b3eccb2c4d8e5d09f52a4dbaaed803993ec
Yannick Uhlmann <augustunderground@protonmail.com>
2021-10-22 17:39:08 +0200

    more tests!

--- src/test.pl
+++ src/test.pl
@@ -7,14 +7,14 @@ opendir(DIR, ".");
 my @pdks = grep(/^[0-9]+$/,readdir(DIR));
 closedir(DIR);
 
-print "Running OP2 for all Nodes\n";
 
-foreach (@pdks) {
-    system "spectre =l ./src/op2-$_.log -I./$_/pdk ./$_/op2/input.scs";
-
-    if ($? == 0) {
-        print "[\e[1;32m ✔ \e[0m] No errors for $_ nm PTM devices.\n";
-    } else {
-        print "[\e[1;31m ✘ \e[0m] $_ nm PTM devices returned with $?.\n"
+foreach my $i (1, 2, 3, 4, 6) {
+    foreach (@pdks) {
+        system "spectre =l ./src/op2-$_.log -I./$_/pdk ./$_/op$i/input.scs";
+        if ($? == 0) {
+            print "[\e[1;32m ✔ \e[0m] No errors for OP$i with $_ nm PTM devices.\n";
+        } else {
+            print "[\e[1;31m ✘ \e[0m] OP$i with $_ nm PTM devices returned with $?.\n"
+        }
     }
 }
